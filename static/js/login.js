a8b5f6e5fc3495c7b6b3ab90e6e524f176f5b83c
Andreas Economides <antroseconomides@hotmail.com>
2019-02-08 13:05:17 +0200

    js: Merge password strength code

--- static/js/login.js
+++ static/js/login.js
@@ -3,44 +3,6 @@ function ClearAlerts() {
         Alert.remove();
 }
 
-function PasswordStrength() {
-    if (!zxcvbn) return;
-
-    const Progress = document.querySelector('div.progress');
-    const Bar = document.querySelector('div.progress-bar');
-    const Span = Progress.querySelector('span');
-    const Password = document.getElementById('registerPassword');
-
-    if (Password.value)
-        Progress.classList.remove('d-none');
-    else
-        Progress.classList.add('d-none');
-
-    const Email = document.getElementById('registerEmail');
-    const { score, feedback } = zxcvbn(Password.value.substring(0, 32),
-        [Email.value, 'ethniki', 'froura', 'portal', 'psifiaki', 'platforma']);
-
-    const Widths = ['w-0', 'w-25', 'w-50', 'w-75', 'w-100'];
-    const Colors = ['bg-danger', 'bg-danger', 'bg-warning', 'bg-info', 'bg-success'];
-    const Strength = ['Πολύ Αδύναμος', 'Αδύναμος', 'Μέτριος', 'Καλός', 'Πολύ Καλός'];
-
-    Bar.classList.remove(...Widths);
-    Bar.classList.remove(...Colors);
-    Bar.classList.add(Colors[score], Widths[score]);
-
-    Span.textContent = Strength[score];
-
-    if (score < 3) {
-        if (feedback.warning)
-            Password.setCustomValidity(feedback.warning);
-        else if (feedback.suggestions)
-            Password.setCustomValidity(feedback.suggestions.join('. '));
-        else
-            Password.setCustomValidity('Password is too weak');
-    } else
-        Password.setCustomValidity('');
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const Login = document.querySelector('form.form-signin');
     const Register = document.querySelector('form.form-register');
@@ -70,21 +32,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
         ClearAlerts();
     });
-
-    const Password1 = document.getElementById('registerPassword');
-    const Password2 = document.getElementById('registerPassword2');
-    const Email = document.getElementById('registerEmail');
-
-    const CheckPasswords = () => {
-        if (Password1.value !== Password2.value)
-            Password2.setCustomValidity('Passwords must match');
-        else
-            Password2.setCustomValidity('');
-    }
-
-    Password1.addEventListener('input', CheckPasswords);
-    Password2.addEventListener('input', CheckPasswords);
-
-    Password1.addEventListener('input', PasswordStrength);
-    Email.addEventListener('input', PasswordStrength);
 })
--- static/js/password_strength.js
+++ static/js/password_strength.js
@@ -3,21 +3,30 @@ function PasswordStrength() {
 
     const Progress = document.querySelector('div.progress');
     const Bar = document.querySelector('div.progress-bar');
-    const Password = document.getElementById('password');
+    const Span = Progress.querySelector('span');
+    const Password = document.getElementById('newPassword1');
+    const Email = document.getElementById('registerEmail');
 
     if (Password.value)
         Progress.classList.remove('d-none');
     else
         Progress.classList.add('d-none');
 
-    const { score, feedback } = zxcvbn(Password.value.substring(0, 32));
+    let Dictionary = ['ethniki', 'froura', 'portal', 'psifiaki', 'platforma'];
+    if (Email)
+        Dictionary.push(Email.value);
+
+    const { score, feedback } = zxcvbn(Password.value.substring(0, 32), Dictionary);
 
     const Widths = ['w-0', 'w-25', 'w-50', 'w-75', 'w-100'];
     const Colors = ['bg-danger', 'bg-danger', 'bg-warning', 'bg-info', 'bg-success'];
+    const Strength = ['Πολύ Αδύναμος', 'Αδύναμος', 'Μέτριος', 'Καλός', 'Πολύ Καλός'];
 
     Bar.classList.remove(...Widths);
     Bar.classList.remove(...Colors);
     Bar.classList.add(Colors[score], Widths[score]);
+
+    Span.textContent = Strength[score];
 
     if (score < 3) {
         if (feedback.warning)
@@ -31,8 +40,9 @@ function PasswordStrength() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const Password1 = document.getElementById('password');
-    const Password2 = document.getElementById('password2');
+    const Password1 = document.getElementById('newPassword1');
+    const Password2 = document.getElementById('newPassword2');
+    const Email = document.getElementById('registerEmail');
 
     const CheckPasswords = () => {
         if (Password1.value !== Password2.value)
@@ -45,4 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
     Password2.addEventListener('input', CheckPasswords);
 
     Password1.addEventListener('input', PasswordStrength);
+
+    if (Email)
+        Email.addEventListener('input', PasswordStrength);
 });
